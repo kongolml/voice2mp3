651877cbdb24a6dcc9c21c4cd5f7f1899c581056
Kostiantyn Golosov <kongol.ml@gmail.com>
2026-02-16 01:44:50 +0100

    deployment to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/voice2mp3/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
